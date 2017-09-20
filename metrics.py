20e3facf95f47c33911eb40d1ad21b1f402ab4ca
Lucas Alves Sobrinho <lucasalves_22@hotmail.com>
2017-09-20 01:56:01 -0300

    Refactoring/simplifying code

--- metrics.py
+++ metrics.py
@@ -1,25 +1,13 @@
 
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.utils import check_array
-
-def _voting_probability(estimators, X, n_classes):
-    n_samples = X.shape[0]
-    proba = np.zeros((n_samples, n_classes))
-    for estimator in estimators:
-        predictions = estimator.predict(X)
-        for i in range(n_samples):
-            proba[i, predictions[i]] += 1
-    return proba
-
-def _predict_probability(estimators, X, n_classes):
-    X = check_array(X, accept_sparse=['csr', 'csc'])
-    all_proba = _voting_probability(estimators, X, n_classes)
-    return all_proba
-
-def _predict(estimators, X, classes):
-    predicted_probability = _predict_probability(estimators, X, len(classes))
-    return classes.take((np.argmax(predicted_probability, axis=1)), axis=0)
 
 def majority_voting_score(X, y, estimators, classes):
-    return accuracy_score(y, _predict(estimators, X, classes))
+    voting_matrix = np.zeros((X.shape[0], len(classes)))
+    for estimator in estimators:
+        predictions = estimator.predict(X)
+        for i in range(X.shape[0]):
+            voting_matrix[i, predictions[i]] += 1
+    voting_score = classes.take(np.argmax(voting_matrix, axis=1))
+    return accuracy_score(y, voting_score)
+
